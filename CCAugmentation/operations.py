0c4497d8491af443dc6ba1ce61a8121171e3eed4
Piotr Juszczyk <piotr.a.juszczyk@gmail.com>
2020-11-17 18:50:21 +0100

    Add randomization of arguments passed to operations

--- CCAugmentation/operations.py
+++ CCAugmentation/operations.py
@@ -62,3 +62,95 @@ class Dropout(Operation):
         for image_and_density_map in images_and_density_maps:
             if random.random() >= self.probability:
                 yield image_and_density_map
+
+
+class RandomArgs(Operation):
+    """
+    Allows running operations with randomized numeral arguments.
+    """
+    def __init__(self, operation, const_args, random_args):
+        """
+        Specify randomization by providing the operation to invoke, const arguments that must be passed as they are and
+        random arguments that will be randomized.
+
+        :param operation: Type of operation that will be invoked. Must come from this, .outputs or .transformations module.
+        :param const_args: Dictionary of constant arguments, i.e. ones whose values (nor names) won't change.
+        :param random_args: Dictionary of randomized arguments specified as (min, max) tuple for each arg name. Values are taken from uniform distribution and are either floats or ints, depending on the types of provided min and max values.
+        """
+        Operation.__init__(self)
+        self.operation = operation
+        self.const_args = const_args
+        self.random_args = random_args
+
+    def _get_op_str(self):
+        """
+        Retrieve string representation of the operation to invoke, in a form that is ready to be computed in eval. That
+        means, a prefix with module name is added when necessary. Only operations from this, .outputs and
+        .transformations modules are supported.
+
+        :return: Potentially prefixed operation name.
+        """
+        import CCAugmentation.outputs as cca_out
+        import CCAugmentation.transformations as cca_trans
+
+        op_name_str = self.operation.__name__
+
+        try:
+            getattr(cca_trans, op_name_str)
+            op_str = f"cca_trans.{op_name_str}"
+        except AttributeError:
+            try:
+                getattr(cca_out, op_name_str)
+                op_str = f"cca_out.{op_name_str}"
+            except AttributeError:
+                op_str = op_name_str
+
+        return op_str
+
+    def _get_const_str(self):
+        """
+        Transform dictionary of constant arguments to string that can be used inside parentheses in eval().
+
+        :return: String representing constant arguments.
+        """
+        const_components = []
+        for k, v in self.const_args.items():
+            v_str = f"'{v}'" if type(v) is str else str(v)
+            const_components.append(f"{k}={v_str}")
+        return ",".join(const_components)
+
+    def _get_rand_str(self):
+        """
+        Randomize, cast and transform to string the arguments that are to be randomized.
+
+        :return: String representing randomized arguments.
+        """
+        rand_components = []
+        for key, (min_val, max_val) in self.random_args.items():
+            val = random.uniform(min_val, max_val)
+            if type(min_val) is int and type(max_val) is int:
+                val = int(val)
+            rand_components.append(f"{key}={str(val)}")
+        return ",".join(rand_components)
+
+    def execute(self, images_and_density_maps):
+        """
+        Create and execute the given operation with a set of constant and randomized arguments.
+
+        :param images_and_density_maps: Iterable of img+DM pairs that are to be used in the operation.
+        :return: Result img+DM pairs from the operation.
+        """
+        # these imports are used in eval(), don't remove them
+        import CCAugmentation.outputs as cca_out
+        import CCAugmentation.transformations as cca_trans
+        _ = cca_out, cca_trans
+
+        op_str = self._get_op_str()
+        const_str = self._get_const_str()
+
+        for image_and_density_map in images_and_density_maps:
+            rand_str = self._get_rand_str()
+            args_str = ",".join([const_str, rand_str]) if const_str and rand_str else const_str + rand_str
+            op = eval(f"{op_str}({args_str})")
+            for result in op.execute([image_and_density_map]):
+                yield result
