a2e45e5c52f9684144fd544eb5b19ab407e1f363
Piotr Juszczyk <piotr.a.juszczyk@gmail.com>
2022-01-02 21:52:43 +0100

    Reuse constants from __init__.py in setup.py

--- setup.py
+++ setup.py
@@ -1,26 +1,35 @@
 import os
 
-from setuptools import setup
+from setuptools import find_packages, setup
+from CCAugmentation import __title__ as TITLE, __version__ as VERSION, __author__ as AUTHOR, __license__ as LICENSE
+
+
+def _parse_requirements():
+    lines = [line.strip() for line in open('requirements.txt')]
+    return [line for line in lines if line and not line.startswith('#')]
+
+
+REQUIREMENTS = _parse_requirements()
 
 DIR_PATH = os.path.dirname(os.path.realpath(__file__))
-with open(os.path.join(DIR_PATH, "README.md"), 'r') as f:
+with open(os.path.join(DIR_PATH, 'README.md'), 'r') as f:
     README = f.read()
 
 setup(
-    name='ccaugmentation',
-    version='0.1.0',
-    packages=['CCAugmentation', 'CCAugmentation.examples'],
+    name=TITLE.lower(),
+    version=VERSION,
+    packages=find_packages(),
     url='https://github.com/pijuszczyk/CCAugmentation',
-    license='MIT',
+    license=LICENSE,
     classifiers=[
         "License :: OSI Approved :: MIT License",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.8",
     ],
-    author='Piotr Juszczyk',
+    author=AUTHOR,
     author_email='piotr.a.juszczyk@gmail.com',
     description='Data preprocessing & augmentation framework, designed for working with crowd counting datasets and ML/DL framework-independent. Supports multitude of simple as well as advanced transformations, outputs and loaders, all of them to be combined using pipelines.',
     long_description=README,
     long_description_content_type='text/markdown',
-    install_requires=['numpy==1.19.3', 'opencv-python', 'matplotlib', 'scipy', 'tqdm']
+    install_requires=REQUIREMENTS
 )
